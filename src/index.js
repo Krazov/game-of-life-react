ca2f50b40a5ae36e0170b419c5e1afa2467cca7f
Jon Krazov <krazov@interia.pl>
2018-01-07 02:38:38 +0100

    Game logic this time
    * Adding keys so React is not wasting any breath about it
    * Calculating new generation
      * Would be faster but functions were copied from Game of Life: With Denizens which use different data structure, and... (long story but I think it’s the last time I am going without specs up front)

--- src/index.js
+++ src/index.js
@@ -18,10 +18,12 @@ class Game extends React.Component {
             <React.Fragment>
                 <h1>{HEADER}</h1>
                 <Board
+                    key={'board'}
                     size={15}
                     running={this.state.running}
                 />
                 <Controls
+                    key={'controls'}
                     toStart={this.startGame.bind(this)}
                     toStop={this.stopGame.bind(this)}
                 />
@@ -38,29 +40,40 @@ class Game extends React.Component {
     }
 }
 
+const directions = [
+    [-1, -1], [0, -1], [1, -1],
+    [-1, 0], /* x */ [1, 0],
+    [-1, 1], [0, 1], [1, 1]
+];
+
 class Board extends React.Component {
     constructor(props) {
         super(props);
 
+        const SIZE = props.size;
+
         this.state = {
-            cells:  Array.from({length: props.size ** 2}, () => 0),
+            cells:  Array.from({length: SIZE ** 2}, () => 0),
+            coords: Array.from({length: SIZE ** 2}, (_, i) => ({x: i % SIZE, y: (i / SIZE) | 0})),
             timeId: 0
         };
     }
 
     render() {
-        const size = {length: this.props.size};
+        const SIZE = this.props.size;
+        const area = {length: SIZE};
 
         return (
             <table>
-                <tbody>{Array.from(size, (_, h) =>
-                    <tr>{
-                        Array.from(size, (_, w) => {
-                            const index = this.props.size * h + w;
+                <tbody>{Array.from(area, (_, h) =>
+                    <tr key={'row-' + h}>{
+                        Array.from(area, (_, w) => {
+                            const index = SIZE * h + w;
                             const cells = this.state.cells;
 
                             return (
                                 <Cell
+                                    key={'cell-' + index}
                                     life={cells[index]}
                                     onClick={() => this.handleClick(index)}
                                 />
@@ -73,7 +86,7 @@ class Board extends React.Component {
     }
 
     componentWillReceiveProps({running = false}) {
-        if (running) {
+        if (running && this.state.timeId === 0) {
             this.startTicking();
         } else {
             this.stopTicking();
@@ -82,16 +95,84 @@ class Board extends React.Component {
 
     startTicking() {
         this.setState({
-            timeId: setTimeout(this.performTic.bind(this), 1000)
+            timeId: setTimeout(this.performTick.bind(this), 1000)
         });
     }
 
-    performTic() {
-        console.log('Tic');
+    performTick() {
+        console.log('Tick');
 
         this.setState({
-            timeId: setTimeout(this.performTic.bind(this), 1000)
+            cells:  Board.getNewGeneration(this.state),
+            timeId: setTimeout(this.performTick.bind(this), 1000)
         });
+    }
+
+    static getNewGeneration({coords, cells}) {
+        return coords
+            .map(Board.getNeighbours(cells, Math.sqrt(cells.length)))
+            .map(Board.countNeighbours(cells))
+            .map(Board.calculateLife(cells));
+    }
+
+    static getNeighbours(cells, size) {
+        return ({x: baseX, y: baseY}, index, coords) =>
+            directions
+                .map(Board.calculatePosition(baseX, baseY, size))
+                .map(Board.getIndexes(coords))
+                .map(Board.extractNeighbours(cells));
+    }
+
+    static calculatePosition(baseX, baseY, size) {
+        return ([currentX, currentY]) => {
+            const x = baseX + currentX;
+            const y = baseY + currentY;
+
+            return [
+                x > -1 ? x < size ? x : 0 : size - 1,
+                y > -1 ? y < size ? y : 0 : size - 1
+            ];
+        };
+    }
+
+    static getIndexes(coords) {
+        return ([nX, nY]) =>
+            coords.findIndex(
+                ({x, y}) => x == nX && y == nY
+            );
+    }
+
+    static extractNeighbours(cells) {
+        return (index) => cells[index];
+    }
+
+    static countNeighbours() {
+        return (neighbours) =>
+            neighbours.reduce(
+                (count, life) => life > 0 ? count + 1 : count, 0
+            );
+    }
+
+    static calculateLife(cells) {
+        return (count, index) => {
+            switch (Board.checkStatus(count)) {
+            case true:
+                return cells[index] + 1;
+            case false:
+                return 0;
+            case null:
+            default:
+                return cells[index];
+            }
+        };
+    }
+
+    static checkStatus(neighbours) {
+        return neighbours == 3
+            ? true
+            : neighbours == 2
+                ? null
+                : false;
     }
 
     stopTicking() {
